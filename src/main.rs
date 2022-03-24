922d7172b7d01005470bb6e25ce7191640f63539
jenoh <memint.contact@gmail.com>
2022-03-24 16:48:15 +0100

    add serde crate and add post route

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
+use rocket_contrib::json::Json;
+use serde::Deserialize;
 
 use rocket_contrib::databases::diesel;
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+struct Metric {
+    loads: (String, String, String),
+}
+
+#[post("/metrics", format = "json", data = "<metric>")]
+fn helloPost(metric: Json<Metric>) -> String {
+    format!("print test {:?}", metric)
+}
 
 #[database("postgres")]
 struct LogsDbConn(diesel::PgConnection);
@@ -16,6 +31,6 @@ fn index() -> &'static str {
 fn main() {
     rocket::ignite()
         .attach(LogsDbConn::fairing())
-        .mount("/", routes![index])
+        .mount("/", routes![index, helloPost])
         .launch();
 }
