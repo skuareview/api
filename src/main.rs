25e7aeaef918b92234785724d1327707cabc8b5e
jenoh <memint.contact@gmail.com>
2022-04-13 20:12:27 +0200

    externalize routes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 // use cdb::get_pool;
 mod db;
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+mod metrics;
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+// #[post("/echo")]
+// async fn echo(req_body: String) -> impl Responder {
+//     HttpResponse::Ok().body(req_body)
+// }
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
@@ -22,9 +23,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(pool.clone())
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/", web::get().to(metrics::hello))
+        // .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
---
+++ src/metrics/mod.rs
@@ -0,0 +1,10 @@
+use actix_web::{get, post, HttpResponse, Responder};
+
+pub async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+// #[post("/echo")]
+// pub async fn echo(req_body: String) -> impl Responder {
+//     HttpResponse::Ok().body(req_body)
+// }
