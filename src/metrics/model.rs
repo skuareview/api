0bd55b589e195e26fc90265f26f07d8fb576463a
jenoh <memint.contact@gmail.com>
2022-04-15 10:35:11 +0200

    add route to setup diesel, post metric work

---
+++ src/metrics/model.rs
@@ -0,0 +1,65 @@
+use diesel::prelude::*;
+
+use crate::diesel::RunQueryDsl;
+use crate::schema::metrics;
+use diesel::{AsChangeset, Queryable};
+use serde_derive::{Deserialize, Serialize};
+
+type DbError = Box<dyn std::error::Error + Send + Sync>;
+
+#[derive(Serialize, Deserialize, Queryable, AsChangeset, Insertable)]
+#[table_name = "metrics"]
+pub struct Metric {
+    pub id: i32,
+    pub load_average_1: Option<String>,
+    pub load_average_2: Option<String>,
+    pub load_average_3: Option<String>,
+    pub memory_used: Option<String>,
+    pub memory_total: Option<String>,
+    pub cpu_temp: Option<String>,
+    pub cpu_load: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Insertable)]
+#[table_name = "metrics"]
+pub struct InsertableMetric {
+    pub load_average_1: Option<String>,
+    pub load_average_2: Option<String>,
+    pub load_average_3: Option<String>,
+    pub memory_used: Option<String>,
+    pub memory_total: Option<String>,
+    pub cpu_temp: Option<String>,
+    pub cpu_load: Option<String>,
+}
+
+impl InsertableMetric {
+    fn from_metric(metric: Metric) -> InsertableMetric {
+        InsertableMetric {
+            load_average_1: metric.load_average_1,
+            load_average_2: metric.load_average_2,
+            load_average_3: metric.load_average_3,
+            memory_used: metric.memory_used,
+            memory_total: metric.memory_total,
+            cpu_temp: metric.cpu_temp,
+            cpu_load: metric.cpu_load,
+        }
+    }
+}
+
+impl Metric {
+    /// Run query using Diesel to insert a new database row and return the result.
+    pub fn insert_new_metric(
+        // prevent collision with `name` column imported inside the function
+        form: &InsertableMetric,
+        conn: &PgConnection,
+    ) -> Result<InsertableMetric, DbError> {
+        // It is common when using Diesel with Actix Web to import schema-related
+        // modules inside a function's scope (rather than the normal module's scope)
+        // to prevent import collisions and namespace pollution.
+        use crate::schema::metrics::dsl::*;
+
+        diesel::insert_into(metrics).values(form).execute(conn)?;
+
+        Ok(form.clone())
+    }
+}
