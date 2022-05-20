8be629ed942c2c39f2b9734091fa1de3b84e1cab
jenoh <memint.contact@gmail.com>
2022-05-20 11:10:05 +0200

    refacto add function to find token

--- src/tests/users_tests.rs
+++ src/tests/users_tests.rs
@@ -1,59 +1,55 @@
 #[cfg(test)]
 mod tests {
-    use crate::agents;
-    use crate::agents::model;
+    use crate::services::response;
     use crate::users;
+    use crate::users::model;
     use actix_web::{test, web, App};
-    use chrono::{Duration, Utc};
     use diesel::prelude::*;
     use diesel::r2d2::{self, ConnectionManager};
-    use jsonwebtoken::{encode, EncodingKey, Header};
+    use rand::Rng;
 
     #[actix_web::test]
-    async fn agents_routes() {
+    async fn users_routes() {
+        // Init
+        let mut rng = rand::thread_rng();
         std::env::set_var("RUST_LOG", "actix_web=debug");
         dotenv::dotenv().ok();
+
         let connspec = std::env::var("DATABASE_URL_TEST").expect("DATABASE_URL_TEST");
         let manager = ConnectionManager::<PgConnection>::new(connspec);
         let pool = r2d2::Pool::builder()
             .build(manager)
             .expect("Failed to create pool.");
-        let conn = pool.get().unwrap();
+
         let mut app = test::init_service(
             App::new()
                 .app_data(web::Data::new(pool.clone()))
-                .service(agents::add_agents),
+                .service(users::register),
         )
         .await;
-
-        // Find user
-        let user: Option<users::model::User> =
-            users::model::User::find_user_with_name("Phantom98".to_owned(), &conn);
-        // Retreive token
-        let token = users::model::User::find_token(user.unwrap().email.clone());
+        let random: String = rng.gen::<i32>().to_string();
+        // Register a user
         let req = test::TestRequest::post()
-            .uri("/agents")
-            .insert_header((
-                actix_web::http::header::AUTHORIZATION,
-                "Bearer ".to_owned() + &token,
-            ))
-            .set_json(&model::AgentName {
-                name: "Good name for an agent".to_string(),
+            .uri("/register")
+            .set_json(&model::Register {
+                name: "Phantom98".to_owned(),
+                email: "phantom@gmail.com".to_owned() + &random,
+                password: "phantom".to_owned(),
             })
             .to_request();
 
-        let resp: model::InsertableAgent = test::call_and_read_body_json(&mut app, req).await;
+        let resp: response::CustomResponse = test::call_and_read_body_json(&mut app, req).await;
 
-        assert_eq!(resp.name, "Good name for an agent");
+        assert_eq!(resp.status, true);
 
-        // // Get a user
+        // Get all roles
         // let req = test::TestRequest::get()
-        //     .uri(&format!("/user/{}", resp.id))
+        //     .uri(&format!("/roles"))
         //     .to_request();
 
-        // let resp: models::User = test::call_and_read_body_json(&mut app, req).await;
+        // let resp: Vec<model::Role> = test::call_and_read_body_json(&mut app, req).await;
 
-        // assert_eq!(resp.name, "Test user");
+        // assert_eq!(resp.first().name, "Admin");
 
         // // Delete new user from table
         // use crate::schema::users::dsl::*;
